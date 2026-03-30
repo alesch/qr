49d76e6c8e3d3749d60785e1ebec866924d18ae5
Alex Schenkman <alex@schenkman.info>
2026-03-30 07:01:42 +0200

    Added generate script

---
+++ generate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+import qrcode
+import qrcode.image.svg
+
+# Initialize the QR Code object
+qr = qrcode.QRCode(
+    version=1,  # Controls the size (1 is 21x21 matrix)
+    error_correction=qrcode.constants.ERROR_CORRECT_M,  # Approx 7% correction
+    box_size=40,  # How many pixels each "box" of the QR code is
+    border=4,  # Thickness of the white border (minimum is 4)
+)
+
+# Add data
+qr.add_data("http://qr.schenkman.info/a")
+qr.make(fit=True)
+
+# Create the image with custom colors
+png_img = qr.make_image(fill_color="black", back_color="white")
+svg_img = qr.make_image(
+    image_factory=qrcode.image.svg.SvgImage, fill_color="black", back_color="white"
+)
+
+# Save the file
+basename = os.path.splitext(sys.argv[1])[0] if len(sys.argv) > 1 else "qr"
+with open(basename + ".png", "wb") as f:
+    png_img.save(f)
+
+with open(basename + ".svg", "wb") as f:
+    svg_img.save(f)
